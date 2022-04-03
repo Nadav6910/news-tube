772be037d899ff2890494f05d1a8053ab972c822
Nadav Shor <nadavs@nadavs-air.home>
2022-04-03 19:03:08 +0300

    added a search bar to the navbar section

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,10 @@ export default function SearchBar() {
 
     return (
         <div className="search-bar-container">
-
+            <form className="search-form" onSubmit={e => e.preventDefault()} role="search">
+                <input className="search-input" type="search" placeholder="Search..." autoFocus />
+                <button className="search-btn" type="submit">Go</button>    
+            </form>
         </div>
     )
 }
